0439905b68ab2e142e8dd1f6b7dc27be30f6430a
bigdre4557@gmail.com <amwilson2019@gmail.com>
2019-06-12 17:43:33 -0400

    Two more to go

--- assignments/this.js
+++ assignments/this.js
@@ -2,9 +2,9 @@
 * in your own words. explain the four principle for the "this" keyword below.
 *
 * 1. The first principle is window console object that will output everything that is used in javascript.
-* 2. JavaScript uses the, "this" keyword that refers to a 
-* 3. 
-* 4. 
+* 2. JavaScript uses the, "this" keyword that reference an object and function on how it is called.
+* 3. When this is used inside of a constructor function it is assign to that object which returns that constructor function.
+* 4. This, can be explicitly binding when it is uses the call and apply methods.
 *
 * write out a code example of each explanation above
 */
@@ -12,11 +12,23 @@
 // Principle 1
 
 // code example for Window Binding
-
+function username(users){
+    console.log(this);
+    return users;
+}
+username('Zyoung');
 // Principle 2
 
 // code example for Implicit Binding
 
+const passwords = {
+    pass_word: 'New password',
+    enter_password: function(pass) {
+        console.log(`Please enter a  ${this.pass_Word}, ${this.pass}`);
+        console.log(this);
+    }
+}
+passwords.enter_password('W@d933!ddE');
 // Principle 3
 
 // code example for New Binding
